89ddc3989c5afc427b88937cedd55734651c0dcb
mitchel buyengo <mitchelongachi63@gmail.com>
2023-07-03 13:00:06 +0300

    completed work

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(a, b) {
+    return a + b;
+
+  }
+  let sum = add(2, 3);
+  console.log(sum);  //Output:5
+  
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  function divide(a, b) {
+    return a / b;
+  }
+  
+  function increment(n) {
+    return n + 1;
+  }
+  
+  
+  function decrement(n) {
+    return n - 1;
+  }
+  
+  function makeInt(n) {
+    return parseInt(n, 10);
+  }
+  
+  function preserveDecimal(n) {
+    return parseFloat(n);
+  }
+  
